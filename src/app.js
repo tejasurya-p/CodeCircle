ea0935700f1149236e8ad928172827d2f9fc4321
tejasury.p <tejasurya.work@gmail.com>
2024-11-27 19:25:13 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -6,10 +6,20 @@ const PORT=3000;
 
 app.listen(PORT,()=>{console.log(`Running on PORT:${PORT}`)});
 
-app.use("/main",(req,res)=>{
-    res.send("Hi Buddy I Am Up And Running ")
+app.use("/",(req,res,next)=>{
+    try{
+
+        throw new exception("hii");
+       
+        
+        
+    }
+    catch{
+        res.send("something gone")
+    }
+    //res.send("Hi Buddy I Am Up And Running ")
 })
 
-app.use("/test",(req,res)=>{
+app.use("/main",(req,res)=>{
     res.send("Hi Buddy I Am Up And Running at /test ")
 })
