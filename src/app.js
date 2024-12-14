1f7b434abb7ba00fb5485817c9d0bcaefb95d33b
tejasury.p <tejasurya.work@gmail.com>
2024-12-14 15:43:11 +0530

    CRUD Ops

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const User = require("./models/user");
 
 const app=express();
 
+app.use(express.json());
+
 const PORT=3000;
 
 dbconnect().then(()=>{
@@ -19,12 +21,11 @@ dbconnect().then(()=>{
   );
 
 app.post('/signup',async (req,res)=>{
-    let user=new User({
-        firstName:"surya",
-        lastName:"teja",
-    })
+    
+    let user=new User(req.body)
+    
     try{
-        await user.save();
+        await user.save()
         res.send('Success');
     }catch(err){
         res.status(403).send('Forbidden');
@@ -34,4 +35,32 @@ app.post('/signup',async (req,res)=>{
     
 })
 
+app.get("/feed", async (req,res)=>{
+    try{
+        let users=await User.find({});
+        res.send(users)
 
+    }
+    catch(err){
+        res.status(404).send("Not Found")
+    }
+})
+
+app.delete("/delete", async (req,res)=>{
+    try{
+        const deleteUser=await User.deleteOne({firstName:req.body.firstName});
+        res.send(deleteUser)
+    }catch(err){
+        res.status(400).send("Un Successfull")
+    }
+})
+
+app.patch("/update", async (req,res)=>{
+    try{
+        const updateUser =await User.updateOne({firstName:req.body.firstName},{firstName:"pk Pawan"})
+        res.send(updateUser)
+    }catch(err){
+        console.log(err);
+        res.send("sorryy mann")
+    }
+})
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,9 @@
 const mongoose=require('mongoose');
+const util = require('util');
+
+
+
+
 
 const userSchema=mongoose.Schema({
     firstName:{
@@ -20,5 +25,8 @@ const userSchema=mongoose.Schema({
 })
 
 const User= mongoose.model('User',userSchema);
+
+console.log(User.constructor.toString());
+console.log(util.inspect(User.constructor, { showHidden: true, depth: 5, colors: true }));
 
 module.exports=User;
